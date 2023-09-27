34a42e87111c9916e46a3ca2a5555e920d146e11
Lexo Liu <me@lexo.cool>
2023-09-27 13:29:03 +0800

    add `find` method

--- together-server/src/message.rs
+++ together-server/src/message.rs
@@ -1,28 +1,63 @@
 use bytestr::ByteStr;
+use futures_util::TryStreamExt;
 use levin::{
+    extract::Query,
     responder::Responder,
     routing::Params,
     utils::{Json, State},
-    Error, StatusCode,
+    Error, StatusCode, Uri,
 };
 use mongodb::{
-    bson::{doc, oid::ObjectId, Bson, DateTime},
+    bson::{doc, oid::ObjectId, Bson, DateTime, Document},
     Database,
 };
 use serde::{Deserialize, Serialize};
 
 use crate::{auth::Auth, parse_oid, ApiMessage};
 
-pub async fn get(database: State<Database>, params: Params) -> levin::Result<impl Responder> {
-    #[derive(Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 
-    struct Message {
-        #[serde(rename(deserialize = "_id"))]
-        id: Bson,
+pub struct Message {
+    #[serde(rename(deserialize = "_id"))]
+    id: Bson,
+    channel: ObjectId,
+    content: String,
+    datetime: DateTime,
+}
+
+pub async fn find(database: State<Database>, uri: Uri) -> levin::Result<impl Responder> {
+    #[derive(Deserialize)]
+    struct FindQuery {
+        start_date: Option<DateTime>,
+        end_date: Option<DateTime>,
         channel: ObjectId,
-        content: String,
-        datetime: DateTime,
+        sender: Option<ObjectId>,
     }
+
+    let collection = database.collection::<Message>("message");
+    let Query(query) = Query::<FindQuery>::from_str(uri.query().unwrap_or(""))?;
+    let mut filter = Document::new();
+    filter.insert("channel", query.channel);
+    let mut daterange = Document::new();
+    if let Some(date) = query.start_date {
+        daterange.insert("$gte", date);
+    }
+
+    if let Some(date) = query.end_date {
+        filter.insert("$lte", date);
+    }
+
+    filter.insert("datetime", daterange);
+
+    if let Some(sender) = query.sender {
+        filter.insert("sender", sender);
+    }
+
+    let result: Vec<Message> = collection.find(filter, None).await?.try_collect().await?;
+    Ok(Json(result))
+}
+
+pub async fn get(database: State<Database>, params: Params) -> levin::Result<impl Responder> {
     let id = params.get("id").ok_or(Error::msg("Missing param `id`"))?;
     let id = parse_oid(id)?;
     let message = database.collection::<Message>("message");
